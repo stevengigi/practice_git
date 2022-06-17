3d0a4010f08f9f319a3a572266c4f6288e31d1dd
steven <steven21470@gmail.com>
2022-06-18 01:19:12 +0800

    add some function and calculate

--- git.c
+++ git.c
@@ -1,4 +1,16 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<complex.h>
+#include<math.h>
+#include<time.h>
 int main(){
  printf("git");
+ double start,end;
+ start=clock();
+ _complex double b=3+4*I;
+ printf("cpow(b): %f +%f i",creal(cpow(b)),cimag(cpow(b)));
+ end=clock();
+ printf("time:%f",end-start);
+ return 0;
 }
